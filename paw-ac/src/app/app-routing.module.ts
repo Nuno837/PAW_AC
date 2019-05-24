59961c083d579a9bb39cfefa307cbfdca272d78d
Ordepsilva <pedro-manuel16@hotmail.com>
2019-05-24 20:24:31 +0100

    Temp Router Add
    routers add para poder andar entre criar campanha e lista campanha

--- paw-ac/src/app/app-routing.module.ts
+++ paw-ac/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CampanhaListComponent } from './campanha/campanha-list/campanha-list.component';
+import { CampanhaCreateComponent } from './campanha/campanha-create/campanha-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CampanhaListComponent},
+  {path: 'create-campanha', component: CampanhaCreateComponent},
+  {path: 'list-campanha', component: CampanhaListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule { 
+
+}
